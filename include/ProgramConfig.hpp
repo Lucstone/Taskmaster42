08cf3de73a36abbe0d4a8f76fb46dc229dd76186
lnaidu <lucasnaidu@hotmail.fr>
2025-10-17 07:50:38 -0500

    struct progconf

--- include/ProgramConfig.hpp
+++ include/ProgramConfig.hpp
@@ -6,7 +6,7 @@
 /*   By: lnaidu <lnaidu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 06:36:06 by lnaidu            #+#    #+#             */
-/*   Updated: 2025/10/15 08:58:39 by lnaidu           ###   ########.fr       */
+/*   Updated: 2025/10/17 07:50:02 by lnaidu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,20 @@
 class ProgramConfig {
     
     private :
-        std::string _name;
-        std::string _cmd;
-        std::vector<std::string> _args;
-
-
-
+        std::string                         _name;
+        std::string                         _cmd;
+        std::string                         _unmask;
+        std::string                         _autorestart;
+        std::string                         _stopsignals;
+        std::string                         _stdout;
+        std::string                         _stderr;
+        std::vector<int>                    _exitcodes;
+        std::map<std::string, std::string>  _env;
+        bool                                _autostart;
+        int                                 _numprocs;
+        int                                 _startretries;
+        int                                 _starttime;
+        int                                 _stoptime;
 
     public :
         ProgramConfig() = default;
