dba859dcb48afb08a63ded7e8e5bbb2ffe3147ea
lnaidu <lucasnaidu@hotmail.fr>
2025-11-03 09:27:55 -0600

    Parsing du Yaml hpp fin mid cpp

--- include/ProgramConfig.hpp
+++ include/ProgramConfig.hpp
@@ -1,4 +1,4 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ProgramConfig.hpp                                  :+:      :+:    :+:   */
@@ -6,18 +6,20 @@
 /*   By: lnaidu <lnaidu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 06:36:06 by lnaidu            #+#    #+#             */
-/*   Updated: 2025/10/17 18:19:48 by lnaidu           ###   ########.fr       */
+/*   Updated: 2025/11/03 06:58:27 by lnaidu           ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #pragma once
 
 # include <string>
 # include <vector>
 # include <map>
-
 # include <iostream>
 # include <cstdlib>   
+# include <yaml-cpp/yaml.h>
+
+
 
 class ProgramConfig {
     
@@ -41,14 +43,38 @@ class ProgramConfig {
     public :
         ProgramConfig();
         ~ProgramConfig();
-        std::string getName(void) const;
-        std::string getCmd(void) const;
-        std::string getUmask(void) const;
-        std::string getAutorestart(void) const;
-        std::string getStopsignals(void) const;
         
+        const std::string&                  getName()        const;
+        const std::string&                  getCmd()         const;
+        const std::string&                  getUmask()       const;
+        const std::string&                  getAutorestart() const;
+        const std::string&                  getStopsignals() const;
+        const std::string&                  getStdoutPath()  const;
+        const std::string&                  getStderrPath()  const;
+        const std::string&                  getWorkingDir()  const;
+        const std::vector<int>&             getExitcodes()   const;
+        const std::map<std::string,std::string>& getEnv()    const;
+        bool                                getAutostart()   const;
+        int                                 getNumprocs()    const;
+        int                                 getStartretries()const;
+        int                                 getStarttime()   const;
+        int                                 getStoptime()    const;
+        void                                setName(const std::string& v);
+        void                                setCmd(const std::string& v);
+        void                                setUmask(const std::string& v);
+        void                                setAutorestart(const std::string& v);
+        void                                setStopsignals(const std::string& v);
+        void                                setStdoutPath(const std::string& v);
+        void                                setStderrPath(const std::string& v);
+        void                                setWorkingDir(const std::string& v);
+        void                                setExitcodes(const std::vector<int>& v);
+        void                                setEnv(const std::map<std::string,std::string>& v);
+        void                                setAutostart(bool v);
+        void                                setNumprocs(int v);
+        void                                setStartretries(int v);
+        void                                setStarttime(int v);
+        void                                setStoptime(int v);
         
-        // voir ave & 
-        
-        
+        static ProgramConfig                fromYAML(const std::string& name, const YAML::Node& node);
+        static std::map<std::string, ProgramConfig> loadAll(const std::string& path);
 };
